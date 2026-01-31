74249efb32f346322efef6415238be499825b036
Conor Foran <foranconor@gmail.com>
2026-02-01 12:36:31 +1300

    Hopeful history fix

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -15,6 +19,26 @@ var (
 	home = filepath.Join(web, "index.html")
 )
 
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func Middleware(next http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for _, middleware := range middlewares {
+		next = middleware(next)
+	}
+	return next
+}
+
 func main() {
 
 	s := http.NewServeMux()
@@ -22,8 +46,11 @@ func main() {
 	s.HandleFunc("GET /", root)
 	s.HandleFunc("GET /{file}", get)
 	s.HandleFunc("GET /assets/", get)
+	s.HandleFunc("GET /pics/{timestamp}", getPic)
 
-	http.ListenAndServe(":6767", s)
+	middleware := Middleware(s, Cors)
+
+	http.ListenAndServe(":6767", middleware)
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
@@ -48,4 +75,47 @@ func serveFile(path string, w http.ResponseWriter, r *http.Request) {
 	ct := mime.TypeByExtension(ext)
 	w.Header().Set("Content-Type", ct)
 	http.ServeFile(w, r, path)
+}
+
+func getPic(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	dir := filepath.Join(web, "pics")
+	// get a list of files in the directory
+	files, err := os.ReadDir(dir)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(files)
+	// get the file name from the request
+	when, err := strconv.Atoi(r.PathValue("timestamp"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// get closest file
+	closest := math.MaxInt64
+	closestFile := files[0].Name()
+	for _, file := range files {
+		millis, err := strconv.Atoi(strings.Split(file.Name(), ".")[0])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		distance := millis - when
+		if distance < 0 {
+			distance = distance * -1
+		}
+		fmt.Println(distance)
+		if distance < closest {
+			closest = distance
+			closestFile = file.Name()
+		}
+
+	}
+	fmt.Println(closestFile)
+	fmt.Println(closest)
+	serveFile(filepath.Join(dir, closestFile), w, r)
 }
--- web/main.js
+++ web/main.js
@@ -1,30 +1,82 @@
 
-let app = document.getElementById("app")
-console.log(app)
+let time = document.getElementById("time")
+let pic = document.getElementById("pic")
 let counter = 0
-const showing = new Date()
+let showing = new Date()
+const refresh = 2000
+let timeout
+let f = 1
 
 function getFilename() {
-  return showing.getFullYear() + "-" + showing.getMonth() + "-" + showing.getDate() + "T" + showing.getHours() + ":" + showing.getMinutes()
+  console.log("getting filename")
+  return showing.getTime()
 }
 
-function shiftTime(amount) {
-  const maybeTime = showing
-  maybeTime.setTime(maybeTime.getTime() + amount * 1000)
+function ticker() {
+  const maybeTime = structuredClone(showing)
+  maybeTime.setTime(maybeTime.getTime() + f * 1000)
   if (maybeTime.getTime() < Date.now()) {
-    maybeTime.setTime(Date.now())
+    showing = maybeTime
   } else {
-    showing.setTime(new Date().getTime())
   }
+}
+
+setInterval(ticker, f * 1000)
+
+function shiftTime(amount) {
+  const maybeTime = structuredClone(showing)
+  console.log(maybeTime)
+  console.log(showing)
+  maybeTime.setTime(maybeTime.getTime() + amount * 1000)
+  if (maybeTime.getTime() > Date.now()) {
+    // showing.setTime(new Date().getTime())
+  } else {
+    showing = maybeTime
+  }
+  console.log(maybeTime)
+  console.log(showing)
 }
 
 updateImage()
 
 function updateImage() {
-  app.innerHTML = `<p>${getFilename()}</p>`
-  setTimeout(updateImage, timer)
+  clearTimeout(timeout)
+  console.log("updating")
+  fetchImage()
+  // time.innerHTML = `<p>${getFilename()}</p>`
+  timeout = setTimeout(updateImage, refresh)
 }
 
-function fetchImage() {
+async function fetchImage() {
+  try {
+    const res = await fetch(`pics/${getFilename()}`)
+    if (res.status === 200) {
+      console.log("got 200")
+      const blob = await res.blob()
+      const url = URL.createObjectURL(blob)
+      pic.src = url
+    } else {
+      throw new Error("Not 200")
+    }
+  } catch (e) {
+    console.log(e)
+  }
 
 }
+
+window.addEventListener("click", function(e) {
+  if (e.target.id === "rewind1") {
+    shiftTime(-60)
+  } else if (e.target.id === "rewind10") {
+    shiftTime(-600)
+  } else if (e.target.id === "rewind100") {
+    shiftTime(-6000)
+  } else if (e.target.id === "forward1") {
+    shiftTime(60)
+  } else if (e.target.id === "forward10") {
+    shiftTime(600)
+  } else if (e.target.id === "forward100") {
+    shiftTime(6000)
+  }
+  updateImage()
+})
