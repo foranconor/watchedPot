db581fe270c55caaa3589aa9df500d3889ad3046
Conor Foran <foranconor@gmail.com>
2026-03-21 16:37:18 +1300

    first go  at speed controls

--- web/main.js
+++ web/main.js
@@ -3,9 +3,10 @@ let time = document.getElementById("time")
 let pic = document.getElementById("pic")
 let counter = 0
 let showing = new Date()
-const refresh = 60 * 1000
+const refresh = 1000
+let speed = 1
 let timeout
-let f = 60
+let f = 1
 
 function getFilename() {
   console.log("getting filename")
@@ -14,34 +15,30 @@ function getFilename() {
 
 function ticker() {
   const maybeTime = structuredClone(showing)
-  maybeTime.setTime(maybeTime.getTime() + f * 1000)
+  maybeTime.setTime(maybeTime.getTime() * 1000 * speed)
   if (maybeTime.getTime() < Date.now()) {
     showing = maybeTime
+
   } else {
   }
 }
 
-setInterval(ticker, f * 1000)
+setInterval(ticker, 1000)
 
 function shiftTime(amount) {
   const maybeTime = structuredClone(showing)
-  console.log(maybeTime)
-  console.log(showing)
   maybeTime.setTime(maybeTime.getTime() + amount * 1000)
   if (maybeTime.getTime() > Date.now()) {
     // showing.setTime(new Date().getTime())
   } else {
     showing = maybeTime
   }
-  console.log(maybeTime)
-  console.log(showing)
 }
 
 updateImage()
 
 function updateImage() {
   clearTimeout(timeout)
-  console.log("updating")
   fetchImage()
   // time.innerHTML = `<p>${getFilename()}</p>`
   timeout = setTimeout(updateImage, refresh)
@@ -51,32 +48,48 @@ async function fetchImage() {
   try {
     const res = await fetch(`pics/${getFilename()}`)
     if (res.status === 200) {
-      console.log("got 200")
       const blob = await res.blob()
       const url = URL.createObjectURL(blob)
       pic.src = url
     } else {
-      throw new Error("Not 200")
+      throw new Error(res.status)
     }
   } catch (e) {
     console.log(e)
   }
-
 }
 
 window.addEventListener("click", function(e) {
-  if (e.target.id === "rewind1") {
-    shiftTime(-60)
-  } else if (e.target.id === "rewind10") {
-    shiftTime(-600)
-  } else if (e.target.id === "rewind100") {
-    shiftTime(-6000)
-  } else if (e.target.id === "forward1") {
-    shiftTime(60)
-  } else if (e.target.id === "forward10") {
-    shiftTime(600)
-  } else if (e.target.id === "forward100") {
-    shiftTime(6000)
+  switch (e.target.id) {
+    case "rewind1":
+      shiftTime(-60)
+      break
+    case "rewind10":
+      shiftTime(-600)
+      break
+    case "rewind100":
+      shiftTime(-6000)
+      break
+    case "forward1":
+      shiftTime(60)
+      break
+    case "forward10":
+      shiftTime(600)
+      break
+    case "forward100":
+      shiftTime(6000)
+      break
+    case "x1":
+      speed = 1
+      break
+    case "x10":
+      speed = 10
+      break
+    case "x100":
+      speed = 100
+      break
+    default:
+      return
   }
   updateImage()
 })
